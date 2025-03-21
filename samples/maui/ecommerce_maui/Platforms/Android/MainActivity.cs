e82d490b6d5ddbbdfd84ec6aaa8ab379fa1a1852
BretJohnson <bret.johnson@microsoft.com>
2025-03-21 12:29:50 -0400

    Add Android keyboard hotkey support to ecommerce sample app

--- samples/maui/ecommerce_maui/Platforms/Android/MainActivity.cs
+++ samples/maui/ecommerce_maui/Platforms/Android/MainActivity.cs
@@ -1,9 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
 
 namespace EcommerceMAUI;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+#if PREVIEWS
+    public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+    {
+        return MauiPreviewApplication.Instance.OnKeyDownForPreviewUI(keyCode, e) || base.OnKeyDown(keyCode, e);
+    }
+#endif
 }
