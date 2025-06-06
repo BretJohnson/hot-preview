cb65e9e35b2e014919ff3dd172758725df6858b8
Bret Johnson <bret@bretajohnson.com>
2025-06-05 22:39:59 -0400

    Add initial (uno default) DevTools project

---
+++ src/ExampleFramework.DevTools/Presentation/SecondPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleFramework.DevTools.Presentation;
+
+public sealed partial class SecondPage : Page
+{
+    public SecondPage()
+    {
+        this.InitializeComponent();
+    }
+}
+
