3f6cdb0ae2aed6d1f00cedbedcc4b469c7fc7501
Bret Johnson <bret@bretajohnson.com>
2025-07-23 00:33:40 -0400

    Remove right-click context menu from Commands in DevToolsApp
    - Add CommandItemTemplate without context menu or RightTapped handler
    - Update NavTreeItemTemplateSelector to use CommandItemTemplate for CommandViewModel instances
    - Commands no longer show 'Update snapshots' context menu on right-click
    - UI Components and other items retain their context menus

--- src/tooling/HotPreview.DevToolsApp/Selectors/NavTreeItemTemplateSelector.cs
+++ src/tooling/HotPreview.DevToolsApp/Selectors/NavTreeItemTemplateSelector.cs
@@ -6,12 +6,14 @@ public class NavTreeItemTemplateSelector : DataTemplateSelector
 {
     public DataTemplate? SectionItemTemplate { get; set; }
     public DataTemplate? DefaultItemTemplate { get; set; }
+    public DataTemplate? CommandItemTemplate { get; set; }
 
     protected override DataTemplate SelectTemplateCore(object item)
     {
         return item switch
         {
             SectionItemViewModel => SectionItemTemplate ?? DefaultItemTemplate!,
+            CommandViewModel => CommandItemTemplate ?? DefaultItemTemplate!,
             _ => DefaultItemTemplate!
         };
     }
