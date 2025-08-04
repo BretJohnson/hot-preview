edf35e60f6455483751c9fcc8992f6106df274ff
Bret Johnson <bret@bretajohnson.com>
2025-08-04 12:28:00 -0400

    Update doc for PreviewCommand

--- src/HotPreview/PreviewCommandAttribute.cs
+++ src/HotPreview/PreviewCommandAttribute.cs
@@ -1,24 +1,23 @@
 namespace HotPreview;
 
 /// <summary>
-/// An attribute that specifies this is a command that can be executed from the preview tools.
-/// Commands are static methods that perform actions and can be invoked from the DevTools UI.
+/// Designates this method as a command that can be executed from the DevTools UI or, soon, as an MCP tool command.
+/// <br/>
+/// Commands typically update global state, changing the way that subsequent previews appear. For instance, commands
+/// could update the UI language for the app or switch the theme between light and dark. Commands normally don't
+/// update UI themselves (but they can - nothing prevents this).
+/// <br/>
+/// Commands for now should have no parameters and return void, though likely we'll add parameter and return support
+/// in the future once we've figured out the desired behavior.
 /// </summary>
+/// <param name="displayName">Optional display name override for the command, determining how it appears in
+/// navigation UI. If not specified, the name of the method is used.</param>
 [AttributeUsage(AttributeTargets.Method)]
-public sealed class PreviewCommandAttribute : Attribute
+public sealed class PreviewCommandAttribute(string? displayName = null) : Attribute
 {
     /// <summary>
-    /// Optional display name for the command, determining how it appears in navigation UI.
-    /// "/" delimiters can be used to indicate hierarchy.
+    /// Optional display name override for the command, determining how it appears in navigation UI.
+    /// If not specified, the name of the method is used.
     /// </summary>
-    public string? DisplayName { get; }
-
-    public PreviewCommandAttribute()
-    {
-    }
-
-    public PreviewCommandAttribute(string? displayName = null)
-    {
-        DisplayName = displayName;
-    }
+    public string? DisplayName { get; } = displayName;
 }
