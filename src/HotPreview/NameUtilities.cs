6dbe642fd32d94bf5e0da3141924639c428d1012
Bret Johnson <bret@bretajohnson.com>
2025-08-04 01:04:49 -0400

    Remove unnecessary using System statements after enabling implicit usings
    - Remove explicit using System statements from all HotPreview project files
    - Leverage implicit usings for cleaner, more concise code
    - Affected files: UIComponentAttribute, UIComponentCategoryAttribute, PreviewAttribute,
      AutoGeneratePreviewAttribute, PreviewCommandAttribute, ControlUIComponentBaseTypeAttribute,
      PageUIComponentBaseTypeAttribute, and NameUtilities
    - Verify project builds successfully with implicit usings enabled

--- src/HotPreview/NameUtilities.cs
+++ src/HotPreview/NameUtilities.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace HotPreview;
 
 public class NameUtilities
--- src/HotPreview/PageUIComponentBaseTypeAttribute.cs
+++ src/HotPreview/PageUIComponentBaseTypeAttribute.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace HotPreview;
 
 [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
